9a1ec04b8e773f70975116acc7df074850a099e8
EDILSON SALVADOR RICCI <edilson.ricci@alper.com.br>
2025-02-28 17:24:14 -0300

    Add files via upload
    Este projeto é uma API RESTful desenvolvida em Spring Boot para gerenciar fornecedores. Ele inclui funcionalidades para listar, buscar, salvar, atualizar e deletar fornecedores.

---
+++ API-RESTFUL-WEB-FORNECEDORES/src/main/java/com/javaricci/ApiRestFulWeb/ApiRestFulWebSpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.javaricci.ApiRestFulWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFulWebSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestFulWebSpringBootApplication.class, args);
+	}
+
+	//WEB
+	//http://localhost:8080/fornecedores
+	
+	//API
+	//http://localhost:8080/api/fornecedores
+	//http://localhost:8080/api/fornecedores/8
+
+}
---
+++ API-RESTFUL-WEB-FORNECEDORES/src/test/java/com/javaricci/BibliaJFA/BibliaJFATests.java
@@ -0,0 +1,13 @@
+package com.javaricci.BibliaJFA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliaJFATests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
